b8cc99b4a6a60557e529020da8bcfebfc2d53873
Avishka Umesh <umesh.avishka@gmail.com>
2023-07-02 17:04:05 +0530

    first commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 300,
+        ),
+        const SizedBox(height: 80),
+        const Text(
+          'Learn Flutter in Fun way!',
+          style: TextStyle(
+            color: Color.fromARGB(255, 237, 223, 252),
+            fontSize: 24,
+          ),
+        ),
+        const SizedBox(height: 30),
+        OutlinedButton(
+          onPressed: () {},
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.white,
+          ),
+          child: const Text('Start Quiz'),
+        )
+      ],
+    ));
+  }
+}
